6edfda725fb76edf8c93f216238c49124086b333
kofthe <hqlmania@gmail.com>
2023-02-06 00:08:08 +0300

    Initial commit

---
+++ src/test/java/kofthe/cmsystem/CmsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kofthe.cmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
